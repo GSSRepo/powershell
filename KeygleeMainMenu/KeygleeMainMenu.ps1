1ea68d86cb97bc8e2313340323b4f2ce111bffd1
Saki <svilen.kostadinov@keygleedispo.com>
2021-04-16 15:20:10 -0700

    second commit

--- KeygleeMainMenu/KeygleeMainMenu.ps1
+++ KeygleeMainMenu/KeygleeMainMenu.ps1
@@ -8,8 +8,8 @@
 function MainMenu {
     Clear-Host
     Write-Color "===================== Choose your Menu =====================" -Color Green
-    Write-Color "1: Press '1' for Netowrk Settings" -Color Green
-    Write-Color "2: Press '2' for Windows Version" -Color Green
+    Write-Color "1: Press '1' for Menu 1" -Color Green
+    Write-Color "2: Press '2' for Menu 2" -Color Green
     Write-Color "3: Press '3' for Windows Update" -Color Green
     Write-Color "4: Press '4' for Run Choco update & Clean Chocolatey Files" -Color Green
     Write-Color "Q: Press 'Q' to quit." -Color Green
